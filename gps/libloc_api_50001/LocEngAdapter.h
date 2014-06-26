4c872ab03d3cea16fc643c6c3b8740e0b84ebdca
Tushar Janefalkar <tusharj@codeaurora.org>
2014-06-26 11:08:12 -0700

    Add support for XTRA_VERSION_CHECK QMI LOC msg
    A new QMI LOC msg is introduced which injects into
    the modem a value that determines whether or not
    the version of XTRA is to be checked
    
    Change-Id: I0ef579332d064713ce73d80e66dc8c6fef1c1638
    CRs-fixed: 649860

--- gps/libloc_api_50001/LocEngAdapter.h
+++ gps/libloc_api_50001/LocEngAdapter.h
@@ -249,7 +249,7 @@ public:
     {
         return mLocApi->getZppFix(zppLoc, tech_mask);
     }
-
+    enum loc_api_adapter_err setXtraVersionCheck(int check);
     virtual void handleEngineDownEvent();
     virtual void handleEngineUpEvent();
     virtual void reportPosition(UlpLocation &location,
@@ -298,6 +298,7 @@ public:
     {
         return mLocApi->getGpsLock();
     }
+
 };
 
 #endif //LOC_API_ENG_ADAPTER_H
