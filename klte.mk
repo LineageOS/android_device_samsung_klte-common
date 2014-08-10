ec91145af0568a5c3dfd52d92ed9f6c607cf3bcd
Matt Filetto <matt.filetto@gmail.com>
2014-08-10 11:59:25 -0700

    klte-common: Add Torch to build
    Change-Id: I68e2785bd138ab93d4fa6c10923a2d9101c1cc9b

--- klte.mk
+++ klte.mk
@@ -101,6 +101,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
