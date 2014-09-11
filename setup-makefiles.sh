afc9f8e02c4f49851c677eaddfb3fe3fd1e74798
Matt Filetto <matt.filetto@gmail.com>
2014-09-11 05:08:11 -0700

    klte-common: add missing entry for kiltevzw in setup-makefiles.sh
    Change-Id: I73944980497aa7d260f54f57412ffff7a83e8809

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -230,7 +230,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter klte kltespr,\$(TARGET_DEVICE)),)
+ifneq (\$(filter klte klteusc kltespr kltevzw,\$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libHevcSwDecoder
