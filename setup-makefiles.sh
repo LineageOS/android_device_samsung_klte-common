07971e3ffe263311cb22306efb49d79a1c6971e7
Matt Filetto <matt.filetto@gmail.com>
2014-09-11 05:07:28 -0700

    klte-common: add klteusc to inclusion list
    Change-Id: Ic125b3838a71467b406a8578d7413c9a1c453121

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -230,7 +230,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter klte kltespr,\$(TARGET_DEVICE)),)
+ifneq (\$(filter klte klteusc kltespr,\$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libHevcSwDecoder
