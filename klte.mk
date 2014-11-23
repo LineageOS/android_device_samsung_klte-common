fadfef8eca332c159d6bb322f329184152a3f485
Ethan Chen <intervigil@gmail.com>
2014-11-23 02:51:52 -0800

    klte-common: Move camera target to device repos
    Change-Id: I448ab54e3f7490ff18a0e14cb5b36c4584a29112

--- klte.mk
+++ klte.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8974
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8974
