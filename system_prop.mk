c7ffd586dec39ad82d3e03804127af2013094ec7
Matt Filetto <matt.filetto@gmail.com>
2014-11-26 19:33:34 -0800

    klte-common: Move KlteRIL to klte-common
    Change-Id: I222b33dd879d1c8cdaf24fb550ce835384c49050

--- system_prop.mk
+++ system_prop.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.lte_vrat_report=1
+    persist.radio.lte_vrat_report=1 \
+    ro.telephony.ril_class=KlteRIL
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
