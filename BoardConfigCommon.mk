0ab1da9e9a720e55d5ca8b23b278a2356cd6ca16
Ethan Chen <intervigil@gmail.com>
2014-12-20 22:56:23 -0800

    Revert "klte-common: Set SELinux to permissive"
    This reverts commit c4674a15394fb596d18a24a86400ae279945dccc.
    
    Change-Id: If43c64b50ea830706decf1b8e2b5ccd727afaf4e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,7 +25,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
