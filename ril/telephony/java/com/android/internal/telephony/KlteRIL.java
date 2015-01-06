ed22a5a6f3f5d5be15b36f473da28d6e6c9ddf62
repo Shareef Ali <shareefalis@gmail.com>
2015-01-06 02:22:36 -0600

    KlteRIL: don't let ril set signal strength type.
    * let framework decide
    
    Change-Id: I2c8337947f63cc5e86ac9d90183ba8bfd378b629

--- ril/telephony/java/com/android/internal/telephony/KlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/KlteRIL.java
@@ -217,7 +217,7 @@ public class KlteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected Object responseSignalStrength(Parcel p) {
-        int numInts = 13;
+        int numInts = 12;
         int response[];
 
         // Get raw data
@@ -232,7 +232,7 @@ public class KlteRIL extends RIL implements CommandsInterface {
         response[4] %= 256;
         response[7] &= 0xff;
 
-        return new SignalStrength(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11], (response[12] != 0));
+        return new SignalStrength(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11], true);
 
     }
 
