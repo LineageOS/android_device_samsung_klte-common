295447113c1ebc49ab7bc8069fe8f757dd162360
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2015-10-10 09:08:23 +0200

    include qcom selinux
    Change-Id: Ibc1d4290aaf06948f61ced4ae0cf921e0032db38

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,7 @@ BOARD_RECOVERY_SWIPE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/samsung/klte-common/sepolicy
