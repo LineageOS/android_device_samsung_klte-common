8ba66fb4589758b1a64f20e1704b6058102f2f80
Ozzy2403 <oskarkratochvil86@yahoo.com>
2015-10-15 16:09:06 -0700

    [DO NOT MERGE]: Disable SELinux
    Change-Id: Ia7140d0cd2c1c80d4811988a3cb4e7960eba1261

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,7 +24,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
@@ -73,12 +73,6 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
-
-# SELinux
--include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/klte-common/sepolicy
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
