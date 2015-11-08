551f0b1b9d9b01679ddaf65912dce1d5ddc60dac
Adam Farden <adam@farden.cz>
2015-11-08 13:06:52 +0100

    Stop the QC-QMI message spam
    Change-Id: I401570796183b53912acfedd00a66a175ec1842f

--- system_prop.mk
+++ system_prop.mk
@@ -41,6 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
+    persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     persist.radio.lte_vrat_report=1 \
     ro.telephony.mms_data_profile=5 \
