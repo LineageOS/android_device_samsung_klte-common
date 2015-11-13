8909ecd7ba61dd4b2c23814ead4a5826457ab5d6
Ethan Chen <intervigil@gmail.com>
2015-11-13 15:54:17 -0800

    klte-common: Move extension library path to device
    Change-Id: I0ef93bf4c42246681e0006b1622b6d935263fe8d

--- system_prop.mk
+++ system_prop.mk
@@ -30,6 +30,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.port=I2C
 
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
