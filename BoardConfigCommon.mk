3ad1f890756f9754e1b4fda190f020d49d6920d4
Ethan Chen <intervigil@gmail.com>
2015-11-14 02:15:18 -0800

    klte-common: Allow device to skip the sensor-permission request
    Change-Id: Ic89ac825a5598a5bb372297cf07529d83b4cb6a3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,9 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 BOARD_SEPOLICY_DIRS += \
     device/samsung/klte-common/sepolicy
 
+# Sensors
+TARGET_NO_SENSOR_PERMISSION_CHECK := true
+
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
 BOARD_WLAN_DEVICE := bcmdhd
