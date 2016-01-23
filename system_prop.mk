22131da94bdbeb3a1e3d650ec4d1706b8df53bce
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-01-23 18:15:18 +0100

    klte-common: set more offload features
    Change-Id: Icf3740f00def48b756de70ef692c9cd0cd716d85

--- system_prop.mk
+++ system_prop.mk
@@ -12,7 +12,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
     use.voice.path.for.pcm.voip=false \
     use.dedicated.device.for.voip=true \
-    audio.deep_buffer.media=true
+    audio.deep_buffer.media=true \
+    audio.offload.pcm.16bit.enable=true \
+    audio.offload.pcm.24bit.enable=true \
+    audio.offload.multiple.enabled=false
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
