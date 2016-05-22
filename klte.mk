966d429ca89b1d3ca6c768748153ccff5f96c2c9
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-05-22 10:01:33 +0200

    klte-common: add performance codecs
    Change-Id: Ifd6a45d9e38c1aea640bb7280163ae754123a446

--- klte.mk
+++ klte.mk
@@ -99,7 +99,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # NFC
 PRODUCT_PACKAGES += \
