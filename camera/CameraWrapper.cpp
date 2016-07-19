6803daababb82ccb80672c851ddb0628bc00ddfa
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-07-19 19:00:25 +0200

    klte-common: more clean up for the wrapper
    *fix turn off torch by setting zsl by blobs
    *set video stabilization to true
    *turn of HDR for back cam as this only work with zsl
    
    Change-Id: Ifb15e64b4c351d7195e5ad3f5d5315419790d0f8

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -122,6 +122,15 @@ static char *camera_fixup_getparams(int __attribute__((unused)) id,
     ALOGV("%s: original parameters:", __FUNCTION__);
     params.dump();
 
+    /* Rear photos: Remove HDR scene mode */
+    if (id == BACK_CAMERA_ID) {
+        params.set(CameraParameters::KEY_SCENE_MODE_VALUES, "auto");
+        params.set(CameraParameters::KEY_SCENE_MODE, "auto");
+    }
+
+    const char *recordHint = params.get(CameraParameters::KEY_RECORDING_HINT);
+    bool videoMode = recordHint ? !strcmp(recordHint, "true") : false;
+
     //Hide nv12-venus from Android.
     if (strcmp (params.getPreviewFormat(), PIXEL_FORMAT_NV12_VENUS) == 0)
           params.setPreviewFormat(params.PIXEL_FORMAT_YUV420SP);
@@ -146,14 +155,19 @@ static char *camera_fixup_getparams(int __attribute__((unused)) id,
         params.set(KEY_VIDEO_HFR_VALUES, hfrModes);
     }
 
-    /* Enforce video-snapshot-supported to true */
-    params.set(CameraParameters::KEY_VIDEO_SNAPSHOT_SUPPORTED, "true");
+    /* Enforce video-stabilization-supported and video-snapshot-supported to true */
+    if (videoMode) {
+        params.set(CameraParameters::KEY_VIDEO_STABILIZATION_SUPPORTED, "true");
+        params.set(CameraParameters::KEY_VIDEO_SNAPSHOT_SUPPORTED, "true");
+    }
 
     ALOGV("%s: Fixed parameters:", __FUNCTION__);
     params.dump();
 
     String8 strParams = params.flatten();
-    return strdup(strParams.string());
+    char *ret = strdup(strParams.string());
+
+    return ret;
 }
 
 static char *camera_fixup_setparams(int id, const char *settings)
@@ -163,16 +177,6 @@ static char *camera_fixup_setparams(int id, const char *settings)
 
     ALOGV("%s: original parameters:", __FUNCTION__);
     params.dump();
-
-    const char *recordingHint = params.get(CameraParameters::KEY_RECORDING_HINT);
-    bool isVideo = recordingHint && !strcmp(recordingHint, "true");
-
-    if (isVideo) {
-        params.set(CameraParameters::KEY_DIS, CameraParameters::DIS_DISABLE);
-        params.set(CameraParameters::KEY_ZSL, CameraParameters::ZSL_OFF);
-    } else {
-        params.set(CameraParameters::KEY_ZSL, CameraParameters::ZSL_ON);
-    }
 
     ALOGV("%s: Fixed parameters:", __FUNCTION__);
     params.dump();
