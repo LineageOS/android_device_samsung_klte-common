3958a0c3765dc31e937eadbbd4a855d183b61cc3
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-12-20 21:05:19 +0100

    klte-common: updates from mm stock CPK1
    Change-Id: I93b840ee6ab9053cbafe4808e8efa57d5fb68903

--- klte.mk
+++ klte.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # NFC
 PRODUCT_PACKAGES += \
