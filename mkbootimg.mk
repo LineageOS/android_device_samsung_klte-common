50b2a9a5f2e4741db317636daa626f621de4515d
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-01-31 07:15:30 -0700

    klte-common: Quiet "KERNEL IS NOT SEANDROID ENFORCING" boot message
    * Let's also take care of the recoveryimage while we are here
    
    Change-Id: Ic26c2d15f9e4b95c2cf006b222a0e0018a20258a

---
+++ mkbootimg.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
+
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target dt image: $@")
+	$(DTBTOOL) $(BOARD_DTBTOOL_ARGS) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
+	$(hide) chmod a+r $@
+	@echo "Made DT image: $@"
+
+## Overload bootimg generation: Same as the original, + magic to quiet bootloader
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
+	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+	@echo "Made boot image: $@"
+
+## Overload recoveryimg generation: Same as the original, + magic to quiet bootloader
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_ramdisk) $(recovery_kernel)
+	$(call build-recoveryimage-target, $@)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
+	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+	@echo "Made recovery image: $@"
+
