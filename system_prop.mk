82cfe043234e5fddf4e7aad1c881aeff0ee07cc8
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-02-03 09:52:24 +0100

    klte-common: define ril property properly
    Change-Id: I74599eeaede5461438b1a3c56429fd45b36ef65f

--- system_prop.mk
+++ system_prop.mk
@@ -59,6 +59,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.mms_data_profile=5 \
     ro.telephony.ril_class=KlteRIL
 
+# Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.telephony.call_ring.multiple=0
+
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sensors=1
