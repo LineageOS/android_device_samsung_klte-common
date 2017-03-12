807302aa7f5664ad562aa98051c267e71881fb50
Kevin F. Haggerty <haggertk@lineageos.org>
2017-03-12 09:25:58 -0600

    klte-common: extract-files: Loop through device blob lists
    * This really should have been in the last commit...
    
    Change-Id: I48961062e3aee7119e1ea0f8a52219d6465d3772

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,8 @@ extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
 # Reinitialize the helper for device
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC"
+for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
+    extract $BLOB_LIST "$SRC"
+done
 
 "$MY_DIR"/setup-makefiles.sh
