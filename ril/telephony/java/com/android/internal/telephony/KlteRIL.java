ac3e90be04ffd28099420f513c4172ad29748069
Paul Keith <javelinanddart@gmail.com>
2017-04-18 20:13:40 -0500

    KlteRIL: Handle RIL_UNSOL_NITZ_TIME_RECEIVED
    * This fixes auto time zone
    * This is related to how it was handled in the
      old SamsungQualcommRIL class (deprecated) but
      the implementation is cleaned up and has been
      fixed to work in more cases, along with being
      closer to the handling in fw/opt/telephony
    * Thanks to gmrt for pointing out flaws in the
      old implementation relating to fixedNitz
    
    Change-Id: I70982873e6304d81794bdd03eb8d9a1c1d3f3a55

--- ril/telephony/java/com/android/internal/telephony/KlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/KlteRIL.java
@@ -57,6 +57,35 @@ public class KlteRIL extends RIL {
         mQANElements = 6;
     }
 
+    private void
+    handleNitzTimeReceived(Parcel p) {
+        String responseVoid = (String) responseString(p);
+        long nitzReceiveTime = p.readLong();
+
+        String fixedNitz = responseVoid;
+        String[] nitzParts = nitz.split(",");
+
+        if (nitzParts.length >= 4) {
+            // 0=date, 1=time+zone, 2=dst, 3+=garbage that confuses ServiceStateTracker (so remove it)
+            fixedNitz = nitzParts[0]+","+nitzParts[1]+","+nitzParts[2];
+        }
+
+        Object result[] = fixedNitz, Long.valueOf(nitzReceiveTime)};
+
+        /* While TelephonyProperties.PROPERTY_IGNORE_NITZ = "telephony.test.ignore.nitz",
+           the actual name of the property is much shorter, so just use that */
+        if (SystemProperties.getBoolean("telephony.test.ignore.nitz", false)) {
+            if (RILJ_LOGD) riljLog("ignoring UNSOL_NITZ_TIME_RECEIVED");
+            return;
+        }
+
+        if (mNITZTimeRegistrant != null) {
+            mNITZTimeRegistrant.notifyRegistrant(new AsyncResult(null, result, null));
+        }
+
+        mLastNITZTimeInfo = result;
+    }
+
     @Override
     public void
     dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
@@ -292,6 +321,9 @@ public class KlteRIL extends RIL {
         int newResponse = response;
 
         switch(response) {
+            case RIL_UNSOL_NITZ_TIME_RECEIVED:
+                handleNitzTimeReceived(p);
+                break;
             case RIL_UNSOL_ON_SS_LL:
                 newResponse = RIL_UNSOL_ON_SS;
                 break;
