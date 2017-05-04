fb4a0246b0c0571888c026dc26c01bb478c9908e
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-05-04 09:44:21 +0200

    klte-common: add audio effects
    Change-Id: I472b1beedb68c7a49aae74f5ecf659753a604e04

--- klte.mk
+++ klte.mk
@@ -56,9 +56,23 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+PRODUCT_PACKAGES += \
+    audiod \
+    audio_policy.msm8974 \
+    audio.a2dp.default \
+    audio.primary.msm8974 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libqcomvisualizer \
+    libqcompostprocbundle \
+    libqcomvoiceprocessing \
+    tinymix
 
 # GPS
 PRODUCT_COPY_FILES += \
