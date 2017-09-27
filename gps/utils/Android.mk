e1215f76fada981192ba3dd63508f128eba0d3a6
Michael Bestas <mkbestas@lineageos.org>
2017-09-27 23:36:53 +0300

    klte-common: gps: Cleanup makefiles and remove useless files
    Change-Id: I66848d2e220817ae61642d7211f7eabf5e4efcea

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,6 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -63,7 +60,4 @@ LOCAL_CLANG := false
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
