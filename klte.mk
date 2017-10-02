46455aa5f867d6eb69f65ad16826e17c5fbf7e2d
Paul Keith <javelinanddart@gmail.com>
2017-10-02 21:32:53 +0200

    klte-common: Use QC soundfx libs
    * These work a lot better than the AOSP ones
    
    Change-Id: Ifb2dbeaacd09329541aeb664a89d4827cf403d77

--- klte.mk
+++ klte.mk
@@ -56,6 +56,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
