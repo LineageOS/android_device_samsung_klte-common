a79e72b246801dbd8cf031361834965d17ab1a01
Bruno Martins <bgcngm@gmail.com>
2017-10-04 13:10:51 +0100

    klte-common: CameraWrapper: Move module to vendor
     * Use LOCAL_MODULE_RELATIVE_PATH as well
    
    Change-Id: I20c7f8b772ade2466dcb2ea7a8b23a07e8d60568

--- camera/Android.mk
+++ camera/Android.mk
@@ -13,8 +13,9 @@ LOCAL_SHARED_LIBRARIES := \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
