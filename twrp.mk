b7d9a63fa52b6a92814d22f6b319c64ba3d8ea96
Kevin F. Haggerty <haggertk@lineageos.org>
2017-10-04 21:29:07 -0600

    klte-common: Update TWRP makefile/compatibility
    * Move TWRP fstab to twrp directory and use TARGET_RECOVERY_DEVICE_DIRS
    * Remove variables set globally in Lineage tree
    * Set TW_NO_LEGACY_PROPS to true in preparation for O compatibility
    
    Change-Id: I8b11be9dbfdb4fd9e1cde7b4a5610d5188fddd9e

--- twrp.mk
+++ twrp.mk
@@ -12,21 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_COPY_FILES += \
-    device/samsung/klte-common/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/klte-common/twrp
 
 RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_VARIANT := twrp
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.191/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 162
-TW_EXCLUDE_SUPERSU := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
 TW_MAX_BRIGHTNESS := 255
 TW_MTP_DEVICE := "/dev/mtp_usb"
 TW_NO_EXFAT_FUSE := true
+TW_NO_LEGACY_PROPS := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_THEME := portrait_hdpi
