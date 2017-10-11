5ee3d1d904e81cca6623c79cbd0e4a48b409ba92
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-11 19:53:28 +0300

    klte-common: Add USB service
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba36

--- klte.mk
+++ klte.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
