67883349d8753b2bec090f63db1b080d4cccc544
Paul Keith <javelinanddart@gmail.com>
2017-10-18 00:02:56 +0200

    KlteRIL: Cleanup RIL class
    * Remove some old hacks
    * Remove an unused ril prop
    * Move to using a prop for mQANElements
    
    Change-Id: Id837ea9b9fc82ebb74212d51974da385b40180a5

--- system_prop.mk
+++ system_prop.mk
@@ -55,8 +55,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
-    persist.radio.lte_vrat_report=1 \
     ro.telephony.mms_data_profile=5 \
+    ro.ril.telephony.mqanelements=6 \
     ro.telephony.ril_class=KlteRIL
 
 # Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
