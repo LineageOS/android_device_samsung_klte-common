2b89f06a2791a5772d9f392fafac906fcaa8d0b3
Paul Keith <javelinanddart@gmail.com>
2017-10-19 02:26:41 +0200

    [DO NOT MERGE - WIP] klte-common: libril: Add Samsung changes
    * Changes in ril.cpp based off of hardware/samsung/libril
    
    [haggertk]: Forward port to ril-caf on lineage-15.0
    *** Everything in ril_unsol_commands_vendor.h is remapped to NULL
        until we figure this crap out
    *** Signal strength stuff isn't ported yet
    *** Ref https://review.lineageos.org/#/c/194096/ for 14.1 version
    
    Change-Id: I59bad9925d141e6cefbc24d4eefdc0c79017852a

---
+++ libril/ril_commands_vendor.h
@@ -0,0 +1,50 @@
+/* //device/libs/telephony/ril_commands.h
+**
+** Copyright 2006, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+    {SAMSUNG_REQUEST_BASE, NULL}, // 10000
+    {RIL_REQUEST_DIAL_EMERGENCY_CALL, radio::dialResponse}, // 10001
+    {RIL_REQUEST_CALL_DEFLECTION, NULL}, // 10002
+    {RIL_REQUEST_MODIFY_CALL_INITIATE, NULL}, // 10003
+    {RIL_REQUEST_MODIFY_CALL_CONFIRM, NULL}, // 10004
+    {RIL_REQUEST_SET_VOICE_DOMAIN_PREF, NULL}, // 10005
+    {RIL_REQUEST_SAFE_MODE, NULL}, // 10006
+    {RIL_REQUEST_SET_TRANSMIT_POWER, NULL}, // 10007
+    {RIL_REQUEST_GET_CELL_BROADCAST_CONFIG, NULL}, // 10008
+    {RIL_REQUEST_GET_PHONEBOOK_STORAGE_INFO, NULL}, // 10009
+    {RIL_REQUEST_GET_PHONEBOOK_ENTRY, NULL}, // 10010
+    {RIL_REQUEST_ACCESS_PHONEBOOK_ENTRY, NULL}, // 10011
+    {RIL_REQUEST_USIM_PB_CAPA, NULL}, // 10012
+    {RIL_REQUEST_LOCK_INFO, NULL}, // 10013
+    {RIL_REQUEST_STK_SIM_INIT_EVENT, NULL}, // 10014
+    {RIL_REQUEST_SET_PREFERRED_NETWORK_LIST, NULL}, // 10015
+    {RIL_REQUEST_GET_PREFERRED_NETWORK_LIST, NULL}, // 10016
+    {RIL_REQUEST_CHANGE_SIM_PERSO, NULL}, // 10017
+    {RIL_REQUEST_ENTER_SIM_PERSO, NULL}, // 10018
+    {RIL_REQUEST_SEND_ENCODED_USSD, NULL}, // 10019
+    {RIL_REQUEST_CDMA_SEND_SMS_EXPECT_MORE, NULL}, // 10020
+    {RIL_REQUEST_HANGUP_VT, NULL}, // 10021
+    {RIL_REQUEST_HOLD, NULL}, // 10022
+    {RIL_REQUEST_SET_SIM_POWER, NULL}, // 10023
+    {10024, NULL}, // 10024
+    {RIL_REQUEST_UICC_GBA_AUTHENTICATE_BOOTSTRAP, NULL}, // 10025
+    {RIL_REQUEST_UICC_GBA_AUTHENTICATE_NAF, NULL}, // 10026
+    {RIL_REQUEST_GET_INCOMING_COMMUNICATION_BARRING, NULL}, // 10027
+    {RIL_REQUEST_SET_INCOMING_COMMUNICATION_BARRING, NULL}, // 10028
+    {RIL_REQUEST_QUERY_CNAP, NULL}, // 10029
+    {RIL_REQUEST_SET_TRANSFER_CALL, NULL}, // 10030
+    {RIL_REQUEST_GET_DISABLE_2G, NULL}, // 10031
+    {RIL_REQUEST_SET_DISABLE_2G, NULL}, // 10032
+    {RIL_REQUEST_REFRESH_NITZ_TIME, NULL}, // 10033
