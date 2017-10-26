1bf14f58892ec7598d3db3650e3fb1ef451f94fe
Kevin F. Haggerty <haggertk@lineageos.org>
2017-10-26 16:37:00 -0600

    klte-common: Set a default rild.libpath in build.prop
    * This is overridden with property_override() in init
      for unified builds
    
    Change-Id: I4173d7c8ffef14ec24da8f61de49bdc61d3a40f2

--- system_prop.mk
+++ system_prop.mk
@@ -56,6 +56,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
+    rild.libpath=/system/vendor/lib/libsec-ril.so \
     ro.telephony.mms_data_profile=5 \
     ro.ril.telephony.mqanelements=6 \
     ro.telephony.ril_class=KlteRIL
