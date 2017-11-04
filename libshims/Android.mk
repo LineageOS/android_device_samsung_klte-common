8450ca7c7dc7ace6e8780593d7a512e9ddae11c7
Kevin F. Haggerty <haggertk@lineageos.org>
2017-11-03 22:24:18 -0600

    klte-common: libshims: Drop libshim_ril
    * O cannot use the old blobbed RIL and this prep makes the rest of
      bringup easier
    
    Change-Id: Ic11b3fcbc6e7923d80eb49ef709ccabe4c81a14d

--- libshims/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ril_shim.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
-LOCAL_MODULE := libshim_ril
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
