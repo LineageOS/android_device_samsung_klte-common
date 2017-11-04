f91f32c463720ae37435b346331d858f99eb1480
Rashed Abdel-Tawab <rashed@linux.com>
2017-11-04 21:30:20 +0200

    klte-common: Add NFC HAL
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba43

--- klte.mk
+++ klte.mk
@@ -126,6 +126,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     com.android.nfc_extras \
     NfcNci \
     Tag
