ee77f5ea18477c793f6c2c0b1ed6b4809199f7c8
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2017-11-05 20:44:05 +0200

    [DNM] klte-common: O bringup
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba15

--- klte.mk
+++ klte.mk
@@ -22,6 +22,7 @@ $(call inherit-product-if-exists, vendor/samsung/klte-common/klte-common-vendor.
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+#DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
@@ -63,7 +64,9 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8974
+    gps.msm8974 \
+    android.hardware.gnss@1.0-impl-qti \
+    libgnss
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/clatd.conf:system/etc/clatd.conf \
@@ -79,6 +82,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8974 \
     libstlport \
     libxml2 \
@@ -89,7 +96,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
 # Doze
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     SamsungDoze
 
 # Fingerprint
@@ -98,7 +105,7 @@ PRODUCT_PACKAGES += \
     fingerprint.msm8974
 
 # FlipFlap
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     FlipFlap
 
 # IPv6 tethering
@@ -108,6 +115,7 @@ PRODUCT_PACKAGES += \
 
 # IR Blaster
 PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
     consumerir.msm8974
 
 # Keylayouts
@@ -117,6 +125,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
+    android.hardware.light@1.0-impl \
     lights.MSM8974
 
 # Media
@@ -126,15 +135,38 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    nfc_nci.msm8974 \
     com.android.nfc_extras \
     NfcNci \
     Tag
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-impl
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Radio
 PRODUCT_PACKAGES += \
     libsecnativefeature \
+    libsecril-client-sap \
     libsecril-client \
-    libshim_ril
+    libshim_cutils_atomic
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -150,10 +182,11 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-impl \
     libnetcmdiface \
-    macloader
-
-PRODUCT_PACKAGES += \
+    wificond \
+    wifilogd \
+    macloader \
     hostapd.accept \
     hostapd.deny \
     hostapd \
