488b4cdfc6c07d52c007523cbac62e25213f95c4
Bruno Martins <bgcngm@gmail.com>
2017-11-07 23:35:45 +0000

    klte-common: Move audio configuration files to vendor
    Change-Id: Ib0609d248c690ae76f3e089f376655da463d6f4c

--- klte.mk
+++ klte.mk
@@ -34,9 +34,9 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
