52569e9c1e297c5a9d3a0382dbf567efc52451da
Bruno Martins <bgcngm@gmail.com>
2017-11-16 22:40:39 +0000

    klte-common: Include DRM service
    Change-Id: I4999809c288c84d6e9c39383ff76f9ee85a97cae

--- klte.mk
+++ klte.mk
@@ -51,7 +51,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
