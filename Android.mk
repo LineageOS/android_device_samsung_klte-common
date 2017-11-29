c526c950ca5747bc2c9703ec20f5856a5396e51c
Paul Keith <javelinanddart@gmail.com>
2017-11-29 16:34:18 +0100

    klte-common: Add kltesprsports to makefile guards
    Change-Id: I77b691da7592ee76095bd0df91534a3efb759734

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter klte kltechn kltechnduo klteduos kltedv kltekdi kltekor kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter klte kltechn kltechnduo klteduos kltedv kltekdi kltekor kltespr kltesprsports klteusc kltevzw,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
