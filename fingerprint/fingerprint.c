09e4b79ed74d8392c9e1439dc1fcb8da7ead945d
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-22 17:04:30 -0700

    klte-common: fingerprint: Bump version to 2.1
    Change-Id: Id355acaf78387917252c56476da0ff8d3828f5d0

--- fingerprint/fingerprint.c
+++ fingerprint/fingerprint.c
@@ -677,7 +677,7 @@ static int fingerprint_open(const hw_module_t* module, const char __unused *id,
     }
 
     vdev->device.common.tag = HARDWARE_DEVICE_TAG;
-    vdev->device.common.version = HARDWARE_MODULE_API_VERSION(2, 0);
+    vdev->device.common.version = FINGERPRINT_MODULE_API_VERSION_2_1;
     vdev->device.common.module = (struct hw_module_t*)module;
     vdev->device.common.close = fingerprint_close;
 
@@ -738,7 +738,7 @@ static struct hw_module_methods_t fingerprint_module_methods = {
 fingerprint_module_t HAL_MODULE_INFO_SYM = {
     .common = {
         .tag                = HARDWARE_MODULE_TAG,
-        .module_api_version = FINGERPRINT_MODULE_API_VERSION_2_0,
+        .module_api_version = FINGERPRINT_MODULE_API_VERSION_2_1,
         .hal_api_version    = HARDWARE_HAL_API_VERSION,
         .id                 = FINGERPRINT_HARDWARE_MODULE_ID,
         .name               = "KLTE Fingerprint HAL",
