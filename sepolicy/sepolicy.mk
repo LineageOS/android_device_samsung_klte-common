ed610b8fc9e4a8ec65b4b501688b4d326cae5ad3
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-25 15:30:21 -0700

    [DO NOT MERGE] klte-common: sepolicy: Rewrite for O
    * WIP
    * KILL that sepolicy/old/ before merging
    * KILL the dontaudits before merging
    
    Change-Id: I6694567fa1c834b262941b9be362c96cbd16625e

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,12 @@
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/klte-common/sepolicy/common \
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung/klte-common/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung/klte-common/sepolicy/private
+
+-include device/qcom/sepolicy/legacy-sepolicy.mk
+-include device/qcom/sepolicy/sepolicy.mk
