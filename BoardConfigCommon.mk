2ac6089dff7221120fedbe6623ac843215fbfbac
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    klte-common: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,16 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 # Extended Filesystem Support
 TARGET_KERNEL_HAVE_EXFAT := true
 
