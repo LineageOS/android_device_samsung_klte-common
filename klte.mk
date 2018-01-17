071ed4dabc88ead01997b135278cbbf81d5d2a78
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 19:07:28 +0200

    klte-common: Move media configs to vendor
    Change-Id: Iffa5bfcac6e363ddd320ef3af5ccdc8c55f7942f

--- klte.mk
+++ klte.mk
@@ -65,8 +65,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
