c2c7e04f0cca1169b8c1f23ba7bdbd6926f501ce
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-17 21:08:19 -0700

    klte-common: Drop GPS configs/PRODUCT_PACKAGES/blobs
    * These have moved to msm8974-common
    
    Change-Id: Ib4cd06549a862f098da692b3a9bd111fd9e9eab7

--- klte.mk
+++ klte.mk
@@ -56,17 +56,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FlipFlap
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8974
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/clatd.conf:system/etc/clatd.conf \
-    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
-
 # IPv6 tethering
 PRODUCT_PACKAGES += \
     ebtables \
