f1fa05ecd042cf117f19f68324a66b66a325a21e
Paul Keith <javelinanddart@gmail.com>
2018-01-18 22:06:22 +0100

    klte-common: Fix nfcee_access.xml copying
    * The Nfc package still expects this in /etc
    
    Change-Id: Id8e9714f96ad1ff145f90838be8e2670a26eb342

--- nfc/product.mk
+++ nfc/product.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    device/samsung/klte-common/nfc/nfcee_access.xml:system/vendor/etc/nfcee_access.xml
+    device/samsung/klte-common/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # NFC Permissions
 PRODUCT_COPY_FILES += \
