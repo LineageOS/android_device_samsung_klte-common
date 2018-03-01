8b8ab824cab84567395f290eb20509acf319b61e
Cal Archer <carcher002@gmail.com>
2018-03-01 22:02:12 +0100

    klte-common: fix camera hal shim
    * fixes binary garbage in CameraParams
    * fixes HDR
    
    Change-Id: Ia33924414b3616682c6eeab6b732ee55df8bb2ce

--- libshims/libshim_camera/camera_shim.c
+++ libshims/libshim_camera/camera_shim.c
@@ -15,19 +15,19 @@
  */
 
 // rt hdr
-const char *_ZN7android16CameraParameters10KEY_RT_HDRE = "rt-hdr";
-const char *_ZN7android16CameraParameters20KEY_SUPPORTED_RT_HDRE = "rt-hdr-values";
-const char *_ZN7android16CameraParameters9RTHDR_OFFE = "off";
-const char *_ZN7android16CameraParameters8RTHDR_ONE = "on";
+const char _ZN7android16CameraParameters10KEY_RT_HDRE[] = "rt-hdr";
+const char _ZN7android16CameraParameters20KEY_SUPPORTED_RT_HDRE[] = "rt-hdr-values";
+const char _ZN7android16CameraParameters9RTHDR_OFFE[] = "off";
+const char _ZN7android16CameraParameters8RTHDR_ONE[] = "on";
 
 // phase af
-const char *_ZN7android16CameraParameters12KEY_PHASE_AFE = "phase-af";
-const char *_ZN7android16CameraParameters22KEY_SUPPORTED_PHASE_AFE = "phase-af-values";
-const char *_ZN7android16CameraParameters7PAF_OFFE = "off";
-const char *_ZN7android16CameraParameters6PAF_ONE = "on";
+const char _ZN7android16CameraParameters12KEY_PHASE_AFE[] = "phase-af";
+const char _ZN7android16CameraParameters22KEY_SUPPORTED_PHASE_AFE[] = "phase-af-values";
+const char _ZN7android16CameraParameters7PAF_OFFE[] = "off";
+const char _ZN7android16CameraParameters6PAF_ONE[] = "on";
 
 // dynamic range control
-const char *_ZN7android16CameraParameters25KEY_DYNAMIC_RANGE_CONTROLE = "dynamic-range-control";
-const char *_ZN7android16CameraParameters35KEY_SUPPORTED_DYNAMIC_RANGE_CONTROLE = "dynamic-range-control-values";
-const char *_ZN7android16CameraParameters7DRC_OFFE = "off";
-const char *_ZN7android16CameraParameters6DRC_ONE = "on";
+const char _ZN7android16CameraParameters25KEY_DYNAMIC_RANGE_CONTROLE[] = "dynamic-range-control";
+const char _ZN7android16CameraParameters35KEY_SUPPORTED_DYNAMIC_RANGE_CONTROLE[] = "dynamic-range-control-values";
+const char _ZN7android16CameraParameters7DRC_OFFE[] = "off";
+const char _ZN7android16CameraParameters6DRC_ONE[] = "on";
