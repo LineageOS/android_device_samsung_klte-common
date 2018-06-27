fc9683cffed1921b29501ae1298517a5d42405c7
Kevin F. Haggerty <haggertk@lineageos.org>
2018-06-27 07:04:04 -0600

    klte-common: Remove hwui properties
    Change-Id: Id5f75dd39c0d385f30e6e8efe7dbe92ad017a688

--- klte.mk
+++ klte.mk
@@ -17,7 +17,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
