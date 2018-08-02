8a4570cb822ce0dab71d3a4910b77df4e22cc24d
T H <socialentry@gmail.com>
2018-08-02 17:06:04 -0400

    klte-common: Use sensors MultiHAL
    Change-Id: I527e72e2e0419931ba21137826774f5b65299f92

--- klte.mk
+++ klte.mk
@@ -95,6 +95,13 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8974
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/vendor/etc/thermal-engine-8974.conf
