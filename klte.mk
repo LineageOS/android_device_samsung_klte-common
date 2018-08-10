98e211ff8ce9fbb63bc466f3a22c61f9dc51a0fe
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    klte-common: Import stock Dalvik heap overrides
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- klte.mk
+++ klte.mk
@@ -16,7 +16,6 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
