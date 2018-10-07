75eed378b3b98318c07401078fc35254c0b264d7
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    klte-common: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Furthermore, with System As Root / is a read-only filesystem
       so we can't create our mountpoints from init script anymore
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

---
+++ config.fs
@@ -0,0 +1,17 @@
+[efs/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_RADIO
+caps: 0
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware-modem/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
