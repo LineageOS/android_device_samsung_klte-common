d6d1f82ee3f1d6b952fc2a3859f03121b9209c56
Bruno Martins <bgcngm@gmail.com>
2019-09-14 22:00:15 +0100

    klte-common: Remove obsolete RIL variant board flag
    There's no CAF variant anymore.
    
    Change-Id: Ief74f8b2aa861b0d16ffab9b52304006008d2444

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,6 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
-TARGET_RIL_VARIANT := caf
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../$(COMMON_PATH)/recovery/recovery_keys.c
