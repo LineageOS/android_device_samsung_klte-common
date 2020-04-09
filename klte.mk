8e198205e7773de36a5c3c1321a9bda949dd104c
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-08 19:22:18 -0600

    klte-common: Set shipping API level in the Lineage product makefile
    Change-Id: I1dc0862a93b3ee2da6673741899f295129f5297c

--- klte.mk
+++ klte.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# Shipping API level
+$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
