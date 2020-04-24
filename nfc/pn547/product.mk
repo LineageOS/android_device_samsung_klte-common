0541957bf52c2fa166c45f66504cde26df4c2082
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-24 10:03:40 -0600

    klte-common: pn547: Update the way we inject the nfc_nci prebuilt
    * The vendor/ makefile guarding is incompatible with the new common
      extract_utils. This way is more straight-forward anyway.
    
    Change-Id: I0322c4be93334dd5a5598cdafef04c3dec3c659d

--- nfc/pn547/product.mk
+++ nfc/pn547/product.mk
@@ -23,4 +23,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     device/samsung/klte-common/nfc/pn547/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    device/samsung/klte-common/nfc/pn547/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    device/samsung/klte-common/nfc/pn547/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    vendor/samsung/klte-common/proprietary/vendor/lib/hw/nfc_nci.msm8974.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/nfc_nci.msm8974.so
