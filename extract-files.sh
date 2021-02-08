50c0701a58fbbb36da9feb9451a3f37256e8943c
Michael Bestas <mkbestas@lineageos.org>
2021-02-08 05:01:48 -0700

    klte-common: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,76 +1,52 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+if [ -z "${DEVICE_COMMON}" ]; then
+    echo ""
+    echo "error: This is a script in a common tree. Please execute" $(basename $0) "from a device tree."
+    echo ""
     exit 1
 fi
-. "$HELPER"
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
 fi
+source "${HELPER}"
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/thermal-engine)
+            sed -i 's|/system/etc|/vendor/etc|g' "${2}"
+            ;;
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i 's|system/etc|vendor/etc|g;
+                    s|/system/lib|/vendor/lib|g;
+                    s|/system/cameradata|/vendor/cameradata|g' "${2}"
+            ;;
+    esac
+}
 
-extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
-extract "$MY_DIR"/common-proprietary-files-pn547.txt "$SRC"
-extract "$MY_DIR"/common-proprietary-files-ril-m.txt "$SRC"
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-COMMON_BLOB_ROOT="$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
-
-MMCAMERA2_SENSOR_MODULES="$COMMON_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i 's|system/etc|vendor/etc|g;
-        s|/system/lib|/vendor/lib|g;
-        s|/system/cameradata|/vendor/cameradata|g' "$MMCAMERA2_SENSOR_MODULES"
-
-THERMAL_ENGINE="$COMMON_BLOB_ROOT"/vendor/bin/thermal-engine
-sed -i 's|/system/etc|/vendor/etc|g' "$THERMAL_ENGINE"
-
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    extract $BLOB_LIST "$SRC"
+for BLOB_LIST in "${MY_DIR}"/common-proprietary-files*.txt; do
+    extract "${BLOB_LIST}" "${SRC}"
 done
 
-./../msm8974-common/extract-files.sh $@
+export BOARD_COMMON=msm8974-common
 
-"$MY_DIR"/setup-makefiles.sh
+"./../../${VENDOR}/${BOARD_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,69 +1,52 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2014
+if [ -z "${DEVICE_COMMON}" ]; then
+    echo ""
+    echo "error: This is a script in a common tree. Please execute" $(basename $0) "from a device tree."
+    echo ""
+    exit 1
+fi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Copyright headers and common guards
-write_headers "klte klteactivexx kltechn kltechnduo klteduos kltedv kltekdi kltekor kltespr kltesprsports klteusc kltevzw"
+# Warning headers and guards
+write_headers "klte klteactivexx kltechn kltechnduo klteduos kltedv kltekdi kltekor"
 
-write_makefiles "$MY_DIR"/common-proprietary-files.txt
+# The standard common blobs
+write_makefiles "${MY_DIR}/common-proprietary-files.txt" true
 
+# Finish
 write_footers
 
 # This is the "common" M radio/QC framework stack. Most variants will inherit
 # this file. If they cannot then they will ship their own stack
 PRODUCTMK="$(echo $PRODUCTMK | sed -e 's|-vendor.mk|-vendor-ril-m.mk|g')"
 write_makefile_header "$PRODUCTMK"
-parse_file_list "$MY_DIR"/common-proprietary-files-ril-m.txt
-write_product_copy_files
+parse_file_list "${MY_DIR}/common-proprietary-files-ril-m.txt"
+write_product_copy_files true
 
-if [ ! -z $VARIANT_COPYRIGHT_YEAR ]; then
-    export INITIAL_COPYRIGHT_YEAR=$VARIANT_COPYRIGHT_YEAR
-fi
+export BOARD_COMMON=msm8974-common
 
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    write_makefiles $BLOB_LIST
-done
-
-write_footers
-
-./../msm8974-common/setup-makefiles.sh $@
+"./../../${VENDOR}/${BOARD_COMMON}/setup-makefiles.sh" "$@"
